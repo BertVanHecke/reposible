02e73132b00009586916d14b7b133ff833c24604
bertvanhecke <bert.vhofficial@gmail.com>
2025-08-18 18:50:52 +0200

    feat(#6): add Supabase client setup and configuration files

---
+++ apps/web/lib/supabase/factories/client.ts
@@ -0,0 +1,13 @@
+import { createBrowserClient } from "@supabase/ssr";
+import { Database } from "../types";
+import { getEnvVar } from "../../../misc/utils";
+
+export function createClient() {
+  return createBrowserClient<Database>(
+    getEnvVar(process.env.NEXT_PUBLIC_SUPABASE_URL, "NEXT_PUBLIC_SUPABASE_URL"),
+    getEnvVar(
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+      "NEXT_PUBLIC_SUPABASE_URL"
+    )
+  );
+}
